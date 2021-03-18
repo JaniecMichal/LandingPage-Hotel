602480bae2386e9bf51c099ff2643a37d2502058
Michal Janiec <michal.janiec95@gmail.com>
2021-03-18 21:48:59 +0100

    Added custom input for dataPicker and Guests Q-ty

--- src/components/MainHeader/SearchingForm/index.js
+++ src/components/MainHeader/SearchingForm/index.js
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-date-picker";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
 import "./style.css";
 import searchIcon from "../../../images/searchIcon.svg";
 
-const SearchingForm = () => (
-  <form className="searchingForm">
-    <div className="inputWrapper ">
-      <label className="searchingForm__label">Check in</label>
-      <input type="date" className="searchingForm__input" />
-    </div>
-    <div className="inputWrapper ">
-      <label className="searchingForm__label">Check out</label>
-      <input type="date" className="searchingForm__input" />
-    </div>
-    <div className="inputWrapper ">
-      <label className="searchingForm__label">Guests</label>
-      <input type="number" className="searchingForm__input" />
-    </div>
-    <div>
-      <button className="searchingForm__button">
-        <span>Search apartament</span>
-        <img src={searchIcon} alt="" />
-      </button>
-    </div>
-  </form>
-);
+const SearchingForm = () => {
+  const [arrivalDate, setArrivalDate] = useState(new Date());
+  const [departureDate, setDepartureDate] = useState(new Date());
+
+  return (
+    <form className="searchingForm">
+      <div className="inputWrapper ">
+        <label className="searchingForm__label">Check in</label>
+        <DatePicker
+          onChange={setArrivalDate}
+          value={arrivalDate}
+          className={"searchingForm__input"}
+          locale={"en-EN"}
+        />
+      </div>
+      <div className="inputWrapper ">
+        <label className="searchingForm__label">Check out</label>
+        <DatePicker
+          onChange={setDepartureDate}
+          value={departureDate}
+          className={"searchingForm__input"}
+          locale={"en-EN"}
+        />
+      </div>
+      <div className="inputWrapper ">
+        <label className="searchingForm__label">Guests</label>
+        <TextField
+          id="standard-number"
+          type="number"
+          variant="outlined"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+      </div>
+      <div>
+        <button className="searchingForm__button">
+          <span>Search apartament</span>
+          <img src={searchIcon} alt="" />
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default SearchingForm;
