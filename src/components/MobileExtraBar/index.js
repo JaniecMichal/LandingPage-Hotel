ad7592cee6843c1179c5d0bb3140ca55cb07f60c
Michal Janiec <michal.janiec95@gmail.com>
2021-03-19 01:52:42 +0100

    Created mobileNavigationBar

---
+++ src/components/MobileExtraBar/index.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import "./style.css";
+import pin from "../../images/pin.svg";
+import menuIco from "../../images/menuIco.svg";
+import searchIcon from "../../images/searchIcon.svg";
+import Socials from "../Socials";
+
+const MobileExtraBar = () => {
+  const [hideNavigation, setHideNavigation] = useState(false);
+
+  const showNavigationBar = () => {
+    setHideNavigation(!hideNavigation);
+  };
+
+  return (
+    <>
+      <nav class={hideNavigation ? "visible" : "unvisible"}>
+        <nav className="mobileNavigationBar">
+          <ul className="mobileNavList">
+            <li>
+              <a href="#apartaments" className="mobileNavList__link">
+                Apartaments
+              </a>
+            </li>
+            <li>
+              <a className="mobileNavList__link"> Restaurant </a>
+            </li>
+            <li>
+              <a className="mobileNavList__link">Gallery </a>
+            </li>
+            <li>
+              <a className="mobileNavList__link">Tips for tips </a>
+            </li>
+            <li>
+              <a className="mobileNavList__link">Special Offers </a>
+            </li>
+            <li>
+              <a className="mobileNavList__link">Contact </a>
+            </li>
+          </ul>
+          <div className="langButtonWrapper">
+            <button
+              className="mobileNavList__language mobileNavList__language--active"
+              active={true}
+            >
+              EN
+            </button>
+            <button className="mobileNavList__language">DE</button>
+          </div>
+          <Socials />
+        </nav>
+      </nav>
+      <footer class="mobileBar">
+        <button class="mobileBar__button">
+          <img src={pin} alt="" />
+        </button>
+        <button class="mobileBar__button">
+          Booking online <img src={searchIcon} alt="" class="searchIcon" />
+        </button>
+        <button
+          class={`mobileBar__button ${
+            hideNavigation ? "mobileBar__button--menuActive" : ""
+          }`}
+          onClick={showNavigationBar}
+        >
+          <img src={menuIco} alt="" />
+        </button>
+      </footer>
+    </>
+  );
+};
+
+export default MobileExtraBar;
